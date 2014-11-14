5a3acf26c94a16a2474619c32cbf6599ddb72cc6
yhamo <hamouda.yassin@hotmail.fr>
2014-11-14 09:26:30 +0100

    testtt2

---
+++ AppAgenc/Presentation/AjouterUneVoiture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppAgenc.Presentation
+{
+    public partial class AjouterUneVoiture : Form
+    {
+        public AjouterUneVoiture()
+        {
+            InitializeComponent();
+        }
+    }
+}
